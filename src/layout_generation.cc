0c79433dd812669326d026bb91e38d595b97c0df
Janis Born <janisborn@gmail.com>
2020-06-25 17:28:33 +0200

    prevent matching of multiple layout vertices to the same target mesh vertex

--- src/layout_generation.cc
+++ src/layout_generation.cc
@@ -2,6 +2,8 @@
 
 #include <polymesh/algorithms/decimate.hh>
 
+#include <set>
+
 void make_layout_by_decimation(const pm::vertex_attribute<tg::pos3>& _t_pos, int _n_vertices, pm::Mesh& _l_m, pm::vertex_attribute<tg::pos3>& _l_pos)
 {
     _l_m.copy_from(_t_pos.mesh());
@@ -25,11 +27,17 @@ std::vector<std::pair<pm::vertex_handle, pm::vertex_handle>> find_matching_verti
     const pm::Mesh& l_m = _l_pos.mesh();
     const pm::Mesh& t_m = _t_pos.mesh();
 
+    std::set<pm::vertex_index> t_matched_v_ids;
+
     std::vector<std::pair<pm::vertex_handle, pm::vertex_handle>> result;
     for (const auto& l_v : l_m.vertices()) {
         auto best_distance_sqr = tg::inf<double>;
         auto best_t_v = pm::vertex_handle::invalid;
         for (const auto& t_v : t_m.vertices()) {
+            // Don't match the same target vertex twice
+            if (t_matched_v_ids.count(t_v.idx)) {
+                continue;
+            }
             const auto distance_sqr = tg::distance_sqr(_l_pos[l_v], _t_pos[t_v]);
             if (distance_sqr < best_distance_sqr) {
                 best_t_v = t_v;
@@ -37,6 +45,7 @@ std::vector<std::pair<pm::vertex_handle, pm::vertex_handle>> find_matching_verti
             }
         }
         result.emplace_back(l_v, best_t_v);
+        t_matched_v_ids.insert(best_t_v.idx);
     }
     return result;
 }
