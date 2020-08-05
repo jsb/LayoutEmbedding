ea0e8522e090ca86e1817a8ebc8334f8346c9bd4
Janis Born <janisborn@gmail.com>
2020-08-05 15:53:38 +0200

    refactor BnB to fix the bug that caused a discrepancy between the computed lower bounds and those stored in the queue

--- library/LayoutEmbedding/BranchAndBound.cc
+++ library/LayoutEmbedding/BranchAndBound.cc
@@ -9,39 +9,13 @@
 
 namespace LayoutEmbedding {
 
-std::set<pm::edge_index> conflicting_paths(const Embedding& _em, const pm::edge_attribute<VirtualPath>& _paths)
-{
-    // Rules:
-    // - Paths must not intersect.
-    // - Paths are allowed to touch other paths at layout vertices (path endpoints).
-    // - Around each layout vertex, the cyclic order of outgoing edges must be consistent with that in the layout.
-
-    const pm::Mesh& l_m = _em.layout_mesh();
-    const pm::Mesh& t_m = _em.target_mesh();
-
-    VirtualPathConflictSentinel vpcs(t_m);
-
-    for (const auto& l_e : l_m.edges()) {
-        const auto& path = _paths[l_e];
-        if (path.empty()) {
-            // Layout edges that have an already embedded path are not candidates.
-            // Therefore, their candidate path is left empty.
-            continue;
-        }
-        LE_ASSERT(path.size() >= 2);
-        vpcs.insert_path(path, l_e);
-    }
-
-    // TODO: detect additional conflicts due to cyclic order!
-
-    return vpcs.global_conflicts;
-}
+using InsertionSequence = std::vector<pm::edge_index>;
 
 struct Candidate
 {
     double lower_bound = std::numeric_limits<double>::infinity();
     double priority = 0.0;
-    std::vector<pm::edge_handle> insertions;
+    InsertionSequence insertions;
 
     bool operator<(const Candidate& _rhs) const
     {
@@ -49,79 +23,118 @@ struct Candidate
     }
 };
 
-struct EmbeddingStats
+struct EmbeddingState
 {
-    double embedded_cost;
-    double unembedded_cost;
-    int num_embedded;
-    int num_conflicting;
-    int num_non_conflicting;
-};
-
-EmbeddingStats calc_cost_lower_bound(const Embedding& _em, const std::vector<pm::edge_handle>& _insertions)
-{
-    // Copy embedding
-    Embedding em(_em);
-    const pm::Mesh& l_m = em.layout_mesh();
-    const pm::Mesh& t_m = em.target_mesh();
-    const pm::vertex_attribute<tg::pos3>& t_pos = em.target_pos();
-
-    // Measure length of "embedded" edges
-    std::set<pm::edge_index> embedded_l_e;
-    double embedded_cost = 0.0;
-    for (const auto& l_e : _insertions) {
-        if (em.is_embedded(l_e)) {
-            embedded_l_e.insert(l_e);
-            embedded_cost += em.embedded_path_length(l_e);
-        }
+    explicit EmbeddingState(const Embedding& _em) :
+        em(_em),
+        candidate_paths(_em.layout_mesh())
+    {
     }
 
-    // Measure length of "unembedded" edges
-    pm::edge_attribute<VirtualPath> candidate_paths(l_m);
-    double unembedded_cost = 0.0;
-    for (const auto& l_e : l_m.edges()) {
-        if (embedded_l_e.count(l_e)) {
-            continue;
-        }
+    explicit EmbeddingState(const EmbeddingState& _es) = default;
 
-        const auto& path = em.find_shortest_path(l_e);
+    Embedding em;
+
+    bool valid = true;
+
+    double embedded_cost = 0.0;
+    double unembedded_cost = 0.0;
+
+    std::set<pm::edge_index> embedded_l_edges;
+    std::set<pm::edge_index> conflicting_l_edges;
+    std::set<pm::edge_index> non_conflicting_l_edges;
+
+    struct CandidatePath
+    {
+        VirtualPath path;
+        double cost = std::numeric_limits<double>::infinity();
+    };
+    pm::edge_attribute<CandidatePath> candidate_paths;
+
+    void extend(const pm::edge_handle& _l_e)
+    {
+        LE_ASSERT(_l_e.mesh == &em.layout_mesh());
+        LE_ASSERT(embedded_l_edges.count(_l_e) == 0);
+
+        auto l_he = _l_e.halfedgeA();
+        auto path = em.find_shortest_path(l_he);
         if (path.empty()) {
-            unembedded_cost = std::numeric_limits<double>::infinity();
-            break;
+            embedded_cost = std::numeric_limits<double>::infinity();
+            valid = false;
+            return;
         }
         else {
-            unembedded_cost += em.path_length(path);
-            candidate_paths[l_e] = path;
+            embedded_cost += em.path_length(path);
+            em.embed_path(l_he, path);
+            embedded_l_edges.insert(_l_e);
         }
     }
 
-    std::set<pm::edge_index> conflicting_l_e = conflicting_paths(em, candidate_paths);
+    void extend(const pm::edge_index& _l_ei)
+    {
+        const auto& l_e = em.layout_mesh().edges()[_l_ei];
+        extend(l_e);
+    }
 
-    EmbeddingStats result;
+    void extend(const InsertionSequence& _seq)
+    {
+        for (const auto& l_e : _seq) {
+            extend(l_e);
+        }
+    }
 
-    result.num_embedded = _insertions.size();
-    result.num_conflicting = conflicting_l_e.size();
-    result.num_non_conflicting = l_m.edges().size() - result.num_embedded - result.num_conflicting;
+    void compute_candidate_paths()
+    {
+        const Embedding& c_em = em; // We don't want to modify the embedding in this method.
 
-    result.embedded_cost = embedded_cost;
-    result.unembedded_cost = unembedded_cost;
+        LE_ASSERT(&candidate_paths.mesh() == &em.layout_mesh());
+        candidate_paths.clear();
+        conflicting_l_edges.clear();
+        non_conflicting_l_edges.clear();
+        unembedded_cost = 0.0;
 
-    return result;
-}
+        VirtualPathConflictSentinel vpcs(c_em.target_mesh());
+
+        for (const auto& l_e : c_em.layout_mesh().edges()) {
+            if (!embedded_l_edges.count(l_e)) {
+                auto l_he = l_e.halfedgeA();
+                auto path = c_em.find_shortest_path(l_he);
+                if (path.empty()) {
+                    candidate_paths[l_e].cost = std::numeric_limits<double>::infinity();
+                }
+                else {
+                    candidate_paths[l_e].path = path;
+                    candidate_paths[l_e].cost = c_em.path_length(path);
+                    vpcs.insert_path(path, l_e);
+                }
+                unembedded_cost += candidate_paths[l_e].cost;
+                // TODO: Early-out if one candidate path is invalid?
+            }
+        }
+
+        conflicting_l_edges = vpcs.global_conflicts;
+        for (const auto& l_e : c_em.layout_mesh().edges()) {
+            if (!embedded_l_edges.count(l_e) && !conflicting_l_edges.count(l_e)) {
+                non_conflicting_l_edges.insert(l_e);
+            }
+        }
+        LE_ASSERT(embedded_l_edges.size() + conflicting_l_edges.size() + non_conflicting_l_edges.size() == em.layout_mesh().edges().size());
+    }
+
+    double cost_lower_bound() const
+    {
+        return embedded_cost + unembedded_cost;
+    }
+};
 
 void branch_and_bound(Embedding& _em, const BranchAndBoundSettings& _settings)
 {
-    const pm::Mesh& l_m = _em.layout_mesh();
-    const pm::Mesh& t_m = _em.target_mesh();
-    const pm::vertex_attribute<tg::pos3>& t_pos = _em.target_pos();
-
+    Candidate best_solution; // Initially empty
     double global_upper_bound = std::numeric_limits<double>::infinity();
-    // TODO: Run heuristic algorithm to find a tighter initial upper bound
+    // TODO: Run heuristic algorithm to find a tighter initial upper bound.
 
-    Candidate best_solution; // initially empty
-
+    // Init priority queue with empty state.
     std::priority_queue<Candidate> q;
-
     {
         Candidate c;
         c.lower_bound = 0.0;
@@ -130,19 +143,20 @@ void branch_and_bound(Embedding& _em, const BranchAndBoundSettings& _settings)
         q.push(c);
     }
 
+    // TODO: Use glow-extras timer instead?
     const auto start_time = std::chrono::steady_clock::now();
 
     while (!q.empty()) {
-        // Timeout
-        if (_settings.timeout > 0.0) {
+        // Time limit
+        if (_settings.time_limit > 0.0) {
             const auto current_time = std::chrono::steady_clock::now();
             const auto interval = current_time - start_time;
             using seconds_double = std::chrono::duration<double>;
             const double elapsed_seconds = std::chrono::duration_cast<seconds_double>(interval).count();
-            if (elapsed_seconds >= _settings.timeout) {
-                std::cout << "Reached time limit of " << _settings.timeout << " s. Terminating." << std::endl;
+            if (elapsed_seconds >= _settings.time_limit) {
+                std::cout << "Reached time limit of " << _settings.time_limit << " s. Terminating." << std::endl;
                 if (std::isinf(global_upper_bound)) {
-                    std::cout << "Warning: No valid solution was found during that time." << std::endl;
+                    std::cout << "Warning: No valid solution was found within that time." << std::endl;
                 }
                 break;
             }
@@ -151,120 +165,28 @@ void branch_and_bound(Embedding& _em, const BranchAndBoundSettings& _settings)
         auto c = q.top();
         q.pop();
 
+        // Early-out based on lower bound cached in c.
         double gap = 1.0 - c.lower_bound / global_upper_bound;
         if (gap <= _settings.optimality_gap) {
             continue;
         }
 
         // Reconstruct the embedding associated with this embedding sequence
-        Embedding em(_em);
+        EmbeddingState es(_em);
+        es.extend(c.insertions);
+        es.compute_candidate_paths();
 
-        // Embed the "already embedded" edges
-        std::set<pm::edge_index> embedded_l_e;
-
-        double embedded_cost = 0.0;
-        for (const auto& l_e : c.insertions) {
-            auto l_he = l_e.halfedgeA();
-            auto path = em.find_shortest_path(l_he);
-            if (path.empty()) {
-                embedded_cost = std::numeric_limits<double>::infinity();
-                break;
-            }
-
-            embedded_cost += em.path_length(path);
-
-            em.embed_path(l_he, path);
-            embedded_l_e.insert(l_e);
-        }
-        if (std::isinf(embedded_cost)) {
-            continue;
-        }
-
-        // Classify the candidate paths: conflicting and nonconflicting
-        pm::edge_attribute<VirtualPath> candidate_paths(l_m);
-
-        double unembedded_cost = 0.0;
-        for (const auto& l_e : l_m.edges()) {
-            if (embedded_l_e.count(l_e)) {
-                continue;
-            }
-
-            const auto& path = em.find_shortest_path(l_e);
-            if (path.empty()) {
-                unembedded_cost = std::numeric_limits<double>::infinity();
-                break;
-            }
-            unembedded_cost += em.path_length(path);
-
-            LE_ASSERT(path.size() >= 2);
-            candidate_paths[l_e] = path;
-        }
-        if (std::isinf(unembedded_cost)) {
-            continue;
-        }
-
-        std::set<pm::edge_index> conflicting_l_e = conflicting_paths(em, candidate_paths);
-
-        // Additional conflicts may arise from inconsistent ordering of outgoing edges around vertices.
-        /*
-        for (const auto& l_he : l_m.halfedges()) {
-            const auto& l_e = l_he.edge();
-            if (embedded_l_e.count(l_e) || conflicting_l_e.count(l_e)) {
-                continue;
-            }
-
-            // Layout edges at the same vertex coming before and after the current edge l_e (in CCW order)
-            const auto& l_he_before = l_he.opposite().next();
-            const auto& l_he_after = l_he.prev().opposite();
-            const auto& l_e_before = l_he_before.edge();
-            const auto& l_e_after = l_he_after.edge();
-
-            CyclicOrderSentinel<pm::edge_index> sentinel({l_e_before, l_e, l_e_after});
-
-            // TODO: use values from covered[el] to find out the embeddings of the surrounding edges.
-            const auto& l_v = l_he.vertex_from();
-            const auto& t_v = em.l_matching_vertex[l_v];
-            for (const auto& t_he : t_v.outgoing_halfedges()) {
-                const auto& covering_l_es = covered[t_he.edge()];
-                for (const auto& c_l_e : covering_l_es) {
-                    sentinel.encounter(c_l_e);
-                }
-                if (!sentinel.valid()) {
-                    break;
-                }
-            }
-            if (!sentinel.valid()) {
-                conflicting_l_e.insert(l_e);
-                break;
-            }
-        }
-        */
-
-        std::set<pm::edge_index> non_conflicting_l_e;
-        for (const auto& l_e : l_m.edges()) {
-            if (!embedded_l_e.count(l_e) && !conflicting_l_e.count(l_e)) {
-                non_conflicting_l_e.insert(l_e);
-            }
-        }
-
-        LE_ASSERT(embedded_l_e.size() + conflicting_l_e.size() + non_conflicting_l_e.size() == l_m.edges().size());
-
-        const double cost_lower_bound = embedded_cost + unembedded_cost;
-
-        // TODO: Shouldn't these values always match? Bug?
-        //LE_ASSERT(cost_lower_bound == c.lower_bound);
-        gap = 1.0 - cost_lower_bound / global_upper_bound;
-        if (gap <= _settings.optimality_gap) {
-            continue;
+        if (c.lower_bound > 0) {
+            LE_ASSERT(es.cost_lower_bound() == c.lower_bound);
         }
 
         std::cout << "|Embd|: " << c.insertions.size();
         std::cout << "    ";
-        std::cout << "|Conf|: " << conflicting_l_e.size();
+        std::cout << "|Conf|: " << es.conflicting_l_edges.size();
         std::cout << "    ";
-        std::cout << "|Ncnf|: " << non_conflicting_l_e.size();
+        std::cout << "|Ncnf|: " << es.non_conflicting_l_edges.size();
         std::cout << "    ";
-        std::cout << "LB: " << cost_lower_bound;
+        std::cout << "LB: " << es.cost_lower_bound();
         std::cout << "    ";
         std::cout << "UB: " << global_upper_bound;
         std::cout << "    ";
@@ -273,49 +195,49 @@ void branch_and_bound(Embedding& _em, const BranchAndBoundSettings& _settings)
         std::cout << "|Q|: " << q.size();
         std::cout << std::endl;
 
-        // Completed layout?
-        if (conflicting_l_e.empty()) {
-            if (cost_lower_bound < global_upper_bound) {
-                global_upper_bound = cost_lower_bound;
+        if (es.cost_lower_bound() < global_upper_bound) {
+            // Completed layout?
+            if (es.conflicting_l_edges.empty()) {
+                global_upper_bound = es.cost_lower_bound();
                 best_solution = c;
                 std::cout << "New upper bound: " << global_upper_bound << std::endl;
             }
-        }
-        else {
-            // Add children to the queue
-            if (cost_lower_bound < global_upper_bound) {
-                for (const auto& l_e : conflicting_l_e) {
-                    Candidate new_c = c;
-                    new_c.insertions.push_back(l_m.edges()[l_e]);
+            else {
+                // Add children to the queue
+                for (const auto& l_e : es.conflicting_l_edges) {
+                    EmbeddingState new_es(es); // Copy
+                    new_es.extend(l_e);
+                    new_es.compute_candidate_paths();
 
-                    EmbeddingStats stats = calc_cost_lower_bound(em, new_c.insertions);
-                    new_c.lower_bound = stats.embedded_cost + stats.unembedded_cost;
-
-                    new_c.priority = new_c.lower_bound * stats.num_conflicting;
-
-                    const double new_gap = 1.0 - new_c.lower_bound / global_upper_bound;
+                    const double new_lower_bound = new_es.cost_lower_bound();
+                    const double new_gap = 1.0 - new_lower_bound / global_upper_bound;
                     if (new_gap > _settings.optimality_gap) {
+                        Candidate new_c;
+                        new_c.insertions = c.insertions;
+                        new_c.insertions.push_back(l_e);
+                        new_c.lower_bound = new_lower_bound;
+                        new_c.priority = new_c.lower_bound * new_es.conflicting_l_edges.size();
                         q.push(new_c);
                     }
                 }
             }
         }
     }
-
     std::cout << "Branch-and-bound optimization completed." << std::endl;
 
     // Apply the victorious embedding sequence to the input embedding
 
     // Edges with predefined insertion sequence
     std::set<pm::edge_index> l_e_embedded;
-    for (const auto& l_e : best_solution.insertions) {
+    for (const auto& l_ei : best_solution.insertions) {
+        const auto l_e = _em.layout_mesh().edges()[l_ei];
         const auto l_he = l_e.halfedgeA();
         const auto path = _em.find_shortest_path(l_he);
         _em.embed_path(l_he, path);
         l_e_embedded.insert(l_e);
     }
     // Remaining edges
-    for (const auto& l_e : l_m.edges()) {
+    for (const auto& l_e : _em.layout_mesh().edges()) {
         if (!l_e_embedded.count(l_e)) {
             const auto l_he = l_e.halfedgeA();
             const auto path = _em.find_shortest_path(l_he);
