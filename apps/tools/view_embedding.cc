7faddb6af03a3ee529e26c1005386f2cdda988d8
Janis Born <janisborn@gmail.com>
2021-02-10 14:51:37 +0100

    use cxxopts in view_embedding app

--- apps/tools/view_embedding.cc
+++ apps/tools/view_embedding.cc
@@ -1,9 +1,5 @@
 /**
   * View a saved embedding file.
-  *
-  * Usage
-  *     ./view_embedding <path_to_lem_file>
-  *
   */
 
 #include <glow-extras/glfw/GlfwContext.hh>
@@ -13,29 +9,50 @@
 #include <LayoutEmbedding/Util/StackTrace.hh>
 #include <LayoutEmbedding/Visualization/Visualization.hh>
 
+#include <cxxopts.hpp>
+
 using namespace LayoutEmbedding;
 namespace fs = std::filesystem;
 
 int main(int argc, char** argv)
 {
     register_segfault_handler();
+
+    fs::path embedding_path;
+
+    cxxopts::Options opts("view_embedding",
+        "View a saved embedding file (.lem).\n");
+    opts.add_options()("e,embedding", "Path to embedding file (.lem).", cxxopts::value<std::string>());
+    opts.add_options()("h,help", "Help.");
+    opts.parse_positional({"embedding"});
+    opts.positional_help("[embedding]");
+    opts.show_positional_help();
+    try {
+        auto args = opts.parse(argc, argv);
+        if (args.count("help")) {
+            std::cout << opts.help() << std::endl;
+            return 0;
+        }
+
+        embedding_path = args["embedding"].as<std::string>();
+    }
+    catch (const cxxopts::OptionException& e) {
+        std::cout << e.what() << "\n\n";
+        std::cout << opts.help() << std::endl;
+        return 1;
+    }
+
     glow::glfw::GlfwContext ctx;
 
     EmbeddingInput input;
     Embedding em(input);
 
     // Load Embedding from file
-    if (argc > 1) {
-        const fs::path filename = argv[1];
-        if (!em.load(filename.parent_path() / filename.stem())) {
-            std::cout << "Embedding file could not be loaded." << std::endl;
-            return 1;
-        }
+    if (!em.load(embedding_path.parent_path() / embedding_path.stem())) {
+        std::cout << "Embedding file could not be loaded." << std::endl;
+        return 1;
+    }
 
-        auto style = default_style();
-        view_embedding(em);
-    }
-    else {
-        std::cout << "Please specify a path to a .lem file." << std::endl;
-    }
+    auto style = default_style();
+    view_embedding(em);
 }
