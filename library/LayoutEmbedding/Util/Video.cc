82bf5be1522da66d8924f043a7d71aa876098e82
Janis Born <janisborn@gmail.com>
2021-02-24 18:42:05 +0100

    increase video quality

--- library/LayoutEmbedding/Util/Video.cc
+++ library/LayoutEmbedding/Util/Video.cc
@@ -26,12 +26,12 @@ void render_video(
         const int _fps,
         const std::string& _filename)
 {
-    // ffmpeg -r 15 -f image2 -i ...%06d.png -vcodec libx264 -pix_fmt yuv420p -y animation.mp4
+    // ffmpeg -r 15 -f image2 -i ...%06d.png -vcodec libx264 -pix_fmt yuv420p -crf 17 -tune animation -y animation.mp4
 
     // Convert png files to mp4
     const auto ffmpeg = "ffmpeg -r " + std::to_string(_fps) + " -f image2 -i "
             + (_path / "%06d.png").string()
-            + " -vcodec libx264 -pix_fmt yuv420p -y "
+            + " -vcodec libx264 -pix_fmt yuv420p -crf 17 -tune animation -y "
             + (_path / _filename).string();
 
     LE_DEBUG_OUT(ffmpeg);
